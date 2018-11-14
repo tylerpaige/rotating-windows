3f53fdf505e15da263add7d14bd0c9b2afaed2ce
Tyler Paige <tyler.paige@dowjones.com>
2018-11-13 22:51:58 -0500

    Moves in a circle

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,93 @@
 require('../scss/index.scss');
+import { roundTo } from './util';
+
+const getPointAlongEllipse = (iteration = 0) => {
+  /*
+    (h, k) = center
+    a = horizontal radius
+    b = vertical radius
+  */
+  const angle = (iteration % 360) * (Math.PI / 180);
+  const a = window.innerWidth / 2;
+  const b = window.innerHeight / 2;
+  // const fooX = (a * Math.cos(angle));
+  // const fooY = (b * Math.sin(angle));
+  // console.log([fooX, fooY]);
+  const x = (a * Math.cos(angle) + a) / window.innerWidth;
+  const y = (b * Math.sin(angle) + b) / window.innerHeight;
+  return {
+    x, 
+    y
+  };
+};
+
+const pointToPositions = ({ x, y }) => {
+  const topLeftScale = [x, y];
+  const topRightScale = [(1 - x), y];
+  const bottomRightScale = [1 - x, (1 - y)];
+  const bottomLeftScale = [x, (1 - y)];
+  return [
+    topLeftScale,
+    topRightScale,
+    bottomRightScale,
+    bottomLeftScale
+  ];
+};
+
+const keyframesToAnimation = (name, keyframes) => {
+  return `@keyframes ${name} { ${keyframes} }`;
+};
+
+const animationNameToApplication = (name, index) => {
+  return `.panel:nth-child(${index + 1}) {
+    animation-name: ${name};
+  }`;
+}
+
+const createAnimations = () => {
+  const numberOfKeyframes = 50;
+  const points = Array.from({ length : numberOfKeyframes })
+    .map((_, index) => getPointAlongEllipse(index / numberOfKeyframes * 360));
+  /* An array of scales (which are arrays) */
+  const positions = points
+    .map(coords => pointToPositions(coords));
+  const keyframes = positions.reduce((acc, panelPosition, keyframeIndex, list) => {
+    const percentage = roundTo(keyframeIndex / list.length * 100, 0);
+    panelPosition.forEach((scaleArr, panelIndex) => {
+      const scale = scaleArr.map(s => roundTo(s, 0)).join(', ');
+      const value = `transform: scale(${scale});`;
+      acc[panelIndex] += `${percentage}% { ${value} }`;
+    });
+    return acc;
+  }, [
+    '',
+    '',
+    '',
+    ''
+  ]);
+  const animationNames = ['topLeft', 'topRight', 'bottomRight', 'bottomLeft'];
+  const animationCss = animationNames.map((name, i) => {
+    // const animationApplication = animationNameToApplication(name, i);
+    // const animationDefinition = keyframesToAnimation(name, keyframes[i]);
+    // return animationApplication + animationDefinition;
+    return keyframesToAnimation(name, keyframes[i]);
+  }).join('');
+  document.body.innerHTML += `<style>${animationCss}</style>`
+
+
+};
 
 const init = () => {
-  
+  // const debugContainer = document.getElementById('debug');
+  // for (let i = 0; i < 360; i++) {
+  //   const coords = getPointAlongEllipse(i);
+  //   const dot = document.createElement('div');
+  //   dot.className = 'dot';
+  //   dot.style.left = `${coords.x * 100}vw`;
+  //   dot.style.top = `${coords.y * 100}vh`;
+  //   debugContainer.appendChild(dot);
+  // }
+  createAnimations()
 };
 
 document.addEventListener('DOMContentLoaded', init);
